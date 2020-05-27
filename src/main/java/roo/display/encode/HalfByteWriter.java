199b859f856105aa3c7721e3ae6a6b17dd2e0d6e
Dawid Kurzyniec <dawidk@google.com>
2020-05-27 21:02:24 +0200

    Removing misplaced comment.

--- src/main/java/roo/display/encode/HalfByteWriter.java
+++ src/main/java/roo/display/encode/HalfByteWriter.java
@@ -3,24 +3,6 @@ package roo.display.encode;
 import java.io.IOException;
 import java.io.OutputStream;
 
-/**
- * Optimized for anti-aliased monochrome content with 4-bit alpha. Runs of
- * extreme values (0x0 and 0xF, i.e. fully opaque and fully transparent) are
- * encoded very efficiently (aiming at the cost of ~1 bit per value). Runs of
- * intermediate values are not encoded nearly as efficiently.
- *
- * Specifically:
- * <ul>
- * <li>runs of 1-7 'F's are encoded as half-byte = 0x8 | the count of 'F's (0x9
- * - 0xF)
- * <li>runs of 1-7 '0's are encoded as half-byte = the count of zeros (0x1 -
- * 0x7)
- * <li>a single value 'V' in the range of 0x1 to 0xE is encoded as 0x0 0xV
- * <li>list of values in the range ox 0x1 to 0xE is encoded as 0x8 <values> 0x0
- * <li>runs of 3-10 values 'V' in the range of 0x1 to 0xE are encoded as 0x8 0x0
- * 0x<count-3> 0xV
- * </ul>
- */
 public class HalfByteWriter {
   private OutputStream os;
   boolean hasHalfByte;
